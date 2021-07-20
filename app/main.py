81e1314c6830cb6a9d93852306f4082b182b0505
Nicholas Herriot <nicholas.herriot@gmail.com>
2021-07-20 15:45:35 +0100

    First commit

---
+++ app/main.py
@@ -0,0 +1,16 @@
+# fastapi_tdd_docker/app/main.py
+
+from fastapi import FastAPI, Depends
+
+from app.config import get_settings, Settings
+
+app = FastAPI()
+
+
+@app.get("/ping")
+async def pong(settings: Settings = Depends(get_settings)):
+    return {
+        "ping": "pong",
+        "environment": settings.environment,
+        "testing": settings.testing
+    }
